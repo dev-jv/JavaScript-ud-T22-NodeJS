787070d0d7fe7e9f607df2bb0739e8545c2d6d4d
dev-jv <dev.javierleon@gmail.com>
2020-11-05 09:23:48 -0500

    five

--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,17 @@ import express from 'express';
 const router = express.Router();
 
 router.get('/', (req, res) => { 
-    res.send('Inicio');
+    res.render('inicio');
 });
 
 router.get('/nosotros', (req, res) => { 
-    res.render('nosotros'); // vista
-});
+
+    const viajes = 'Viaje a Alemania';
+
+    res.render('nosotros', {
+        viajes
+    }); // vista
+});  
 
 export default router;
 
