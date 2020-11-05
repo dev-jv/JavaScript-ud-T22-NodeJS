6bc13a54dea7d6bccb9d8b74b2397dd02254c1bd
dev-jv <dev.javierleon@gmail.com>
2020-11-04 19:06:31 -0500

    two

--- index.js
+++ index.js
@@ -1,8 +1,32 @@
-const express = require('express'); // Importamos express y lo asignamos
+
+// ------------------------------------ Importamos express y lo asignamos
+// const express = require('express'); // commonjs
+import express from 'express'; // Imports > module 
 
 const app = express(); // Función para ejecutar expres, que también asignamos
 
 const port = process.env.PORT || 4000;
+
+app.get('/', (req, res) => { // req: lo que enviamos | res: lo que express nos responde
+
+    // res.send('Un villano que acabará con la paz mundial!');
+
+    // res.json({
+    //     id:1
+    // });
+
+    // res.render();
+
+    res.send('Inicio');
+});
+
+app.get('/nosotros', (req, res) => { 
+    res.send('Nosotros');
+});
+
+app.get('/contacto', (req, res) => { 
+    res.send('Contacto');
+});
 
 app.listen(port, ()=>{ 
     console.log(`El Servidor esta funcionando en el puerto ${port}`)
