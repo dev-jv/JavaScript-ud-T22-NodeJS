420caf2b9f5f56a827f6b346e50bed2229c93437
dev-jv <dev.javierleon@gmail.com>
2020-11-04 23:29:20 -0500

    three

--- index.js
+++ index.js
@@ -1,36 +1,15 @@
+import express from 'express';
+import router from './routes/index.js';
 
-// ------------------------------------ Importamos express y lo asignamos
-// const express = require('express'); // commonjs
-import express from 'express'; // Imports > module 
 
-const app = express(); // Función para ejecutar expres, que también asignamos
+const app = express();
 
 const port = process.env.PORT || 4000;
 
-app.get('/', (req, res) => { // req: lo que enviamos | res: lo que express nos responde
-
-    // res.send('Un villano que acabará con la paz mundial!');
-
-    // res.json({
-    //     id:1
-    // });
-
-    // res.render();
-
-    res.send('Inicio');
-});
-
-app.get('/nosotros', (req, res) => { 
-    res.send('Nosotros');
-});
-
-app.get('/contacto', (req, res) => { 
-    res.send('Contacto');
-});
+app.use('/', router); // Agrega el router
 
 app.listen(port, ()=>{ 
     console.log(`El Servidor esta funcionando en el puerto ${port}`)
-}) // Inicia el servidor con app, y utiliza el método listen; listen indica el puerto y con un callback ejecuta un msj
-
+})
 
 
