4c74ccf04dc5478757edcb1982a55ac29f8ead8f
dev-jv <dev.javierleon@gmail.com>
2020-11-09 15:10:07 -0500

    twenty-seven

--- server/index.js
+++ server/index.js
@@ -1,6 +1,9 @@
 import express from 'express';
 import router from './routes/index.js';
 import db from './config/db.js'
+import dotenv from 'dotenv';
+
+dotenv.config({path: 'variables.env'})
 
 const app = express();
 
@@ -21,13 +24,17 @@ app.use((req, res, next) => {
     next();
 });
 
-app.use(express.urlencoded({extend: true})); // Agregar body parser par leer los datos del formulario
+app.use(express.urlencoded({extend: true})); // Agregar body parser para leer los datos del formulario
 
 app.use(express.static('public')); // Define la carp. public
 
 app.use('/', router); // Agrega el router
 
-app.listen(port, ()=>{ 
+const host = process.env.HOST || '0.0.0.0';
+// const port = process.env.PORT || 3000;
+
+
+app.listen(port, host , ()=>{ 
     console.log(`El Servidor esta funcionando en el puerto ${port}`)
 })
 
